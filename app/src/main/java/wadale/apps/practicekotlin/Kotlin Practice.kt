8a967bb2d1a56ffd2eba4be4f0b90c378c795ac8
dale-code <boluwadaledara2018@gmail.com>
2021-03-23 15:55:35 +0100

    first practice

--- app/src/main/java/wadale/apps/practicekotlin/Kotlin Practice.kt
+++ app/src/main/java/wadale/apps/practicekotlin/Kotlin Practice.kt
@@ -1,1 +1,18 @@
 package wadale.apps.practicekotlin
+
+class Person (name: String, Surname: String) {
+
+    init {
+        // initializer block
+    }
+}
+fun add (x:Int, y:Int) : Int  = x + y
+
+
+
+
+
+
+
+
+
